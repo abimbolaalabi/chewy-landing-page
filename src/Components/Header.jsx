c277f2e771f25053e2619602b11c9aa38d92cebe
abimbolaalabi <ibrahimade92@gmail.com>
2025-08-01 11:08:39 +0100

    git commit -m Worked on Header, Hero and LastSec

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,10 @@
 import Button from "./Button";
 const Header = () => {
     return (
-        <div className="flex flex-col bg-[#1c49c2] w-full h-[7rem] p-3  text-white font-bold">
+        <div className="flex flex-col bg-[#1c49c2] w-full h-[7.5rem] p-3 fixed text-white font-bold">
             {/* upper part */}
             <div className=" flex  justify-between items-center w-full py-2">
-               <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/Chewy_pet_food_logo.svg/2560px-Chewy_pet_food_logo.svg.png" className="h-[3rem] object-contain" />
+               <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/Chewy_pet_food_logo.svg/2560px-Chewy_pet_food_logo.svg.png" className="w-[10rem] object-contain" />
                {/* search */}
                <div className="bg-white w-[22rem] p-3 flex justify-between items-center rounded-md">
                 <input type="text" placeholder="Search" className="border-none outline-none justify-between text-gray-900"/>
@@ -18,8 +18,9 @@ const Header = () => {
                 </div>
                <Button  text="Use App" style=" border-2 border-white text-white px-3 py-2 rounded-full"/> 
                {/* help*/}
-            <div className="flex items-center gap-1">
-                <i class="fa-regular fa-message"></i>
+            <div className="flex items-center gap-1 text-white">
+                <svg xmlns="http://www.w3.org/2000/svg"  width="20" height="20" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M7.791 19c-.326-.132-.507-.196-.607-.183c-.124.015-.304.147-.664.41c-.634.466-1.432.8-2.617.771c-.599-.014-.898-.021-1.032-.249c-.134-.227.033-.542.367-1.172c.463-.874.756-1.874.312-2.675c-.766-1.144-1.417-2.5-1.512-3.963a19 19 0 0 1 0-2.388c.261-4.019 3.433-7.22 7.415-7.484c1.356-.09 2.774-.09 4.132 0c3.965.263 7.127 3.438 7.412 7.433"/><path d="M14.698 21.647c-2.51-.16-4.51-2.117-4.674-4.573a11 11 0 0 1 0-1.46c.165-2.456 2.164-4.412 4.674-4.573c.856-.055 1.75-.055 2.604 0c2.51.161 4.51 2.117 4.674 4.574c.032.48.032.978 0 1.459c-.06.894-.47 1.723-.953 2.422c-.28.49-.095 1.1.197 1.634c.21.385.316.578.231.717s-.273.143-.65.152c-.747.018-1.25-.187-1.65-.471c-.227-.161-.34-.242-.418-.251s-.232.052-.54.174a3 3 0 0 1-.89.196a21 21 0 0 1-2.605 0"/></g></svg>
+                {/* <i class="fa-regular fa-message"></i> */}
                <p>24/7Help</p>
                 <i class="fa-solid fa-chevron-down"></i>
             </div>  
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,36 +1,39 @@
- import Button from "./Button"
-const Hero =() => {
+import Button from "./Button"
+const Hero = () => {
     return (
-        <div className="mt-8">
+        <div className="mt-20">
+            <div className="w-full text-center py-3 shadow-md font-bold" >
+                <p>$20 eGift card $49+ on 1st order* use WELCOME</p>
+            </div>
             <div className="w-full h-[20rem]  object-cover">
-               <img src="images\heroSec.avif" alt="" className="object-cover h-full rounded-md bg" />
+                <img src="images\heroSec.avif" alt="" className="object-cover h-full rounded-md bg" />
             </div>
             <div className="w-full h-[20vh] mt-3 bg-[#f3f5f9] rounded-md flex  items-center justify-between px-5">
-              <div className="flex justify-center items-center gap-3">
-                 <p className="font-bold text-2xl">Hey, Friend!</p>
-                 <Button text="Sign in" style={ "border-2 border-none text-white px-5 py-2 rounded-full bg-blue-700 "}/>
-              </div>
-              <div className="flex gap-5">
-                <div className="w-[20rem] h-[80px] bg-[#fff] flex px-4 items-center rounded-md">
-                <div>
-                    <img src="/images/water-removebg-preview.png" alt="" className="w-12" />
+                <div className="flex justify-center items-center gap-3">
+                    <p className="font-bold text-2xl">Hey, Friend!</p>
+                    <Button text="Sign in" style={"border-2 border-none text-white px-5 py-2 rounded-full bg-blue-700 "} />
                 </div>
-                <div>
-                    <p>Save 35% on first order</p>
-                    <p>Set up an Autoship</p>
+                <div className="flex gap-5">
+                    <div className="w-[20rem] h-[80px] bg-[#fff] flex px-4 items-center rounded-md">
+                        <div>
+                            <img src="/images/water-removebg-preview.png" alt="" className="w-12" />
+                        </div>
+                        <div>
+                            <p>Save 35% on first order</p>
+                            <p>Set up an Autoship</p>
+                        </div>
+                    </div>
+                    <div className="w-[20rem] h-[80px] bg-[#ffffff] flex justify-center items-center rounded-md">
+                        <div>
+                            <img src="\images\bottle-removebg-preview.png" alt="" className="w-12" />
+                        </div>
+                        <div>
+                            <p>Save 20% on first pharmacy order</p>
+                            <p>Fill a prescription</p>
+                        </div>
+                    </div>
                 </div>
-              </div>
-              <div className="w-[20rem] h-[80px] bg-[#ffffff] flex justify-center items-center rounded-md">
-                <div>
-                    <img src="\images\bottle-removebg-preview.png" alt="" className="w-12" />
-                </div>
-                <div>
-                    <p>Save 20% on first pharmacy order</p>
-                    <p>Fill a prescription</p>
-                </div>
-              </div>
-              </div>
-              
+
             </div>
         </div>
     )
